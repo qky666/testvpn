3e3cf467fea6265b7153fc38f1b52d18d7efbfaf
jose.torrecilla@mtp.es <jose.torrecilla@mtp.es>
2022-03-28 12:11:52 +0200

    TestVPN

--- src/test/java/testvpn/TestVPN.java
+++ src/test/java/testvpn/TestVPN.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 
 public class TestVPN {
     @Test
-    void testVpn() {
+    void testVpn() throws InterruptedException {
         Configuration.browser = "chrome";
         Configuration.baseUrl = "https://google.com";
         Configuration.headless = true;
         Configuration.remote = "http://chrome_vpn:4444";
         Selenide.open();
+        Thread.sleep(3000);
+        Selenide.screenshot("screenshot.png");
     }
 }
